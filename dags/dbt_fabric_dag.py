baf1fd980c8e3c7f17e0ce2d42fbd750f5d63b5c
jimzip <a@b.c>
2024-10-27 15:39:33 -0400

    test

---
+++ dags/dbt_fabric_dag.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+from datetime import datetime
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+
+DEFAULT_DBT_ROOT_PATH = Path(__file__).parent.parent / "dbt_fabric_test"
+DBT_ROOT_PATH = Path(os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH))
+profile_config = ProfileConfig(
+     profile_name="dbt_fabric_test",
+     target_name="dev",
+     profiles_yml_filepath=DBT_ROOT_PATH / "profiles.yml",
+)
+
+dbt_fabric_dag = DbtDag(
+     project_config=ProjectConfig(DBT_ROOT_PATH,),
+     operator_args={"install_deps": True},
+     profile_config=profile_config,
+     schedule_interval="@daily",
+     start_date=datetime(2023, 9, 10),
+     catchup=False,
+     dag_id="dbt_fabric_dag",
+)
