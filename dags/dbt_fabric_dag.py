8f4cc7a145f5465161fe2950fec97ef785a721f1
jimzip <a@b.c>
2024-10-27 17:58:31 -0400

    y

--- dags/dbt_fabric_dag.py
+++ dags/dbt_fabric_dag.py
@@ -1,41 +1,38 @@
-"""
-A basic dbt DAG that shows how to run dbt commands via the BashOperator
+import os
+from pathlib import Path
+from datetime import datetime
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
 
-Follows the standard dbt seed, run, and test pattern.
-"""
+DEFAULT_DBT_ROOT_PATH = Path(__file__).parent.parent / "dags" / "dbt_fabric_test"
+DBT_ROOT_PATH = Path(os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH))
+profile_config = ProfileConfig(
+     profile_name="dbt_fabric_test",
+     target_name="dev",
+     profiles_yml_filepath=DBT_ROOT_PATH / "profiles.yml",
+)
 
-from pendulum import datetime
-
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+dbt_fabric_dag = DbtDag(
+     project_config=ProjectConfig(DBT_ROOT_PATH,),
+     operator_args={"install_deps": True},
+     profile_config=profile_config,
+     schedule_interval="@daily",
+     start_date=datetime(2023, 9, 10),
+     catchup=False,
+     dag_id="dbt_fabric_dag",
+)
 
 # We're hardcoding this value here for the purpose of the demo, but in a production environment this
 # would probably come from a config file and/or environment variables!
-DBT_PROJECT_DIR = "/dags/dbt_fabric_test"
+# DBT_PROJECT_DIR = "/dags/dbt_fabric_test"
 
+# dbt_run = BashOperator(
+#     task_id="dbt_run",
+#     bash_command=f"dbt run --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+# )
 
-with DAG(
-    "dbt_fabric_dag",
-    start_date=datetime(2020, 12, 23),
-    description="A sample Airflow DAG to invoke dbt runs using a BashOperator",
-    schedule_interval=None,
-    catchup=False
-) as dag:
-    # This task loads the CSV files from dbt/data into the local postgres database for the purpose of this demo.
-    # In practice, we'd usually expect the data to have already been loaded to the database.
-    # dbt_seed = BashOperator(
-    #     task_id="dbt_seed",
-    #     bash_command=f"dbt seed --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
-    # )
+# dbt_test = BashOperator(
+#     task_id="dbt_test",
+#     bash_command=f"dbt test --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+# )
 
-    dbt_run = BashOperator(
-        task_id="dbt_run",
-        bash_command=f"dbt run --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
-    )
-
-    # dbt_test = BashOperator(
-    #     task_id="dbt_test",
-    #     bash_command=f"dbt test --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
-    # )
-
-    dbt_run
+# dbt_run
